075a3f6c81f1634730cdf0985fe0c72a7dd44628
yuriy_y <yuriy.yurchenko@playtech.com>
2020-08-12 13:24:55 +0300

    added jwt token support

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+function generateJWT() {
+    var username = document.getElementById('username').value;
+    var casinoName = document.getElementById('casinoName').value;
+    var sessionToken = document.getElementById('sessionToken').value;
+
+    if (!username || !casinoName || !sessionToken) {
+        if (!username) alert('Username required');
+        if (!casinoName) alert('Casino Name required');
+        if (!sessionToken) alert('Session Token required');
+        return;
+    }
+
+    var requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'token': sessionToken,
+        },
+        body: JSON.stringify({ username, casinoName }),
+    };
+
+
+    return fetch('https://admin.kievimsdev1.ukraine.ptec/chat-admin/api/jwt', requestOptions)
+        .then(this.handleResponse)
+        .then(resp => {
+            var $token = document.getElementById('token');
+            $token.value = resp.data.jwtToken;
+
+            return resp;
+        });
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        var data = text && JSON.parse(text);
+        if (!response.ok) {
+            var error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
+
 function login() {
     var token = document.getElementById('token').value;
 
@@ -19,14 +63,11 @@ function login() {
     };
 
     loadChat();
-    appendImageAfterClick();
 }
 
 function guest() {
     loadChat();
-    appendImageAfterClick();
 }
-
 
 function loadChat() {
     var script = document.createElement('script');
@@ -36,22 +77,15 @@ function loadChat() {
     document.body.parentNode.appendChild(script);
 }
 
-function appendImageAfterClick() {
-    var $img = document.createElement('img');
-    var $footer = document.getElementById('footer');
-    //$img.src = 'https://hsto.org/getpro/habr/comment_images/3f1/f54/a1b/3f1f54a1bca0277a20b65310d155fe2d.gif';
-    //$footer.appendChild($img);
-}
-
 function addTag(){
      var tag = document.getElementById('tag').value;
-    
+
      if(!tag) {
         alert('please enter the tag');
         return;
     }
     window.zE('webWidget', 'chat:addTags',[tag]);
-    
+
 }
 
 function updatePath(){
@@ -60,8 +94,7 @@ function updatePath(){
      window.zE('webWidget', 'updatePath', {
         url: new_url,
         title: title
-    });   
-    
+    });
 }
 
 function removeTag(){
